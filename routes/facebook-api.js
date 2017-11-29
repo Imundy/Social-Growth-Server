c0203b3e38d5db6d28f42d33fb5ead3c3ffc4451
imundy <mundycore@gmail.com>
2017-11-28 18:13:51 -0800

    Add api structure

---
+++ routes/facebook-api.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/accounts/add', (req, res) => {
+
+});
+
+router.post('/accounts/remove', (req, res) => {
+  
+});
+
+router.get('/accounts', (req, res) => {
+
+});
+
+router.post('settings/reviews', (req, res) => {
+
+});
+
+router.get('settings/reviews', (req, res) => {
+  
+});
+
+router.post('settings/comments/like', (req, res) => {
+  
+});
+
+router.get('settings/comments/like', (req, res) => {
+  
+});
+
+router.post('settings/comments/hide', (req, res) => {
+  
+});
+
+router.get('settings/comments/hide', (req, res) => {
+  
+});
+
+module.exports = router;
---
+++ routes/instagram-api.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/accounts/add', (req, res) => {
+
+});
+
+router.post('/accounts/remove', (req, res) => {
+  
+});
+
+router.get('/accounts', (req, res) => {
+
+});
