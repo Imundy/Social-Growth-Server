babbdf6cd4caded974c17c783bc133e33f37cd87
david nakayama <david.m.nakayama@vanderbilt.edu>
2017-12-08 22:06:56 -0800

    Parse int challenge.

--- routes/facebook.js
+++ routes/facebook.js
@@ -5,7 +5,7 @@ router.get('/webhook', (req, res) => {
   const verify = req.query['hub.verify_token'];
 
   if (verify === 'quothTheRavenNevermore') {
-    return res.json(req.query['hub.challenge']);
+    return res.json(parseInt(req.query['hub.challenge'], 10));
   }
 
   return res.status(200).send();
