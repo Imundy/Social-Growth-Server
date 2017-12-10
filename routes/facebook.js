7052d5bb67cf947d5a01c35e940882459e587e9c
david nakayama <david.m.nakayama@vanderbilt.edu>
2017-12-10 14:52:51 -0800

    Add log to post.

--- routes/facebook.js
+++ routes/facebook.js
@@ -8,12 +8,11 @@ router.get('/webhook', (req, res) => {
     return res.json(parseInt(req.query['hub.challenge'], 10));
   }
 
-  console.log('webhook ', JSON.stringify(req.body));
-
   return res.status(200).send();
 });
 
 router.post('/webhook', (req, res) => {
+  console.log('webhook ', JSON.stringify(req.body));
   return res.status(200).send();
 });
 
