bc9a48468da30114216b11061b6cf543f390eb1e
david nakayama <david.m.nakayama@vanderbilt.edu>
2017-12-10 16:54:19 -0800

    Add webhook support for facebook.

--- routes/facebook.js
+++ routes/facebook.js
@@ -12,8 +12,45 @@ router.get('/webhook', (req, res) => {
 });
 
 router.post('/webhook', (req, res) => {
-  console.log('webhook ', JSON.stringify(req.body));
+  if (req.body.entry) {
+    req.body.entry.forEach(entry => {
+      const change = entry.changes.find(x => x.verb === 'add');
+      if (change.field === 'feed') {
+        likePost(change.value);
+        hidePost(change.value);
+      }
+    })
+  }
   return res.status(200).send();
 });
+
+// {
+//   "entry": [
+//     {
+//       "changes": [
+//         {
+//           "field": "feed",
+//           "value": {
+//             "recipient_id": "184126008807648",
+//             "from": {
+//               "id": "115999789188722"
+//             },
+//             "item": "post",
+//             "post_id": "184126008807648_189146338305615",
+//             "verb": "remove",
+//             "created_time": 1512949967
+//           }
+//         }
+//       ],
+//       "id": "184126008807648",
+//       "time": 1512949981
+//     }
+//   ],
+//   "object": "page"
+// }
+
+likePost(post) {
+
+}
 
 module.exports = router;
